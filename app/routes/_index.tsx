247b3ec91ee61561e88d9cc3fd47cf01f317cb01
Blake Netzeband <blake.netzeband@gmail.com>
2024-02-01 18:34:09 -0700

    copypasta grid component:

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,6 +2,10 @@ import type { MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { register } from "~/api/auth";
 
+import { Container, Grid, SimpleGrid, Skeleton, rem } from '@mantine/core';
+
+const PRIMARY_COL_HEIGHT = rem(300);
+
 export const meta: MetaFunction = () => {
   return [
     { title: "Genre Expositor" },
@@ -15,12 +19,28 @@ export async function loader() {
 
 export default function Index() {
   // Get the data from the Loader function
-  const res = useLoaderData<typeof loader>();
-  console.log({ res })
+  const authTroubles = useLoaderData<typeof loader>();
+  console.log(authTroubles)
+
+  const SECONDARY_COL_HEIGHT = `calc(${PRIMARY_COL_HEIGHT} / 2 - var(--mantine-spacing-md) / 2)`;
+
 
   return (
-    <main>
-      <h1>Genre Expositor</h1>
-    </main>
+    <Container my="md" component="main">
+      <SimpleGrid cols={{ base: 1, sm: 2 }} spacing="md">
+        <Skeleton height={PRIMARY_COL_HEIGHT} radius="md" animate={false} />
+        <Grid gutter="md">
+          <Grid.Col>
+            <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} />
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} />
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} />
+          </Grid.Col>
+        </Grid>
+      </SimpleGrid>
+    </Container>
   );
 }
