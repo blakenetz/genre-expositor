601123cb887845fd3e8a3b8c6aa2afbe0ae548b9
Blake Netzeband <blake.netzeband@gmail.com>
2024-02-04 16:26:11 -0700

    setup search results

--- app/api/spotify.ts
+++ app/api/spotify.ts
@@ -1,4 +1,5 @@
 import { getToken } from "./auth";
+import { SearchResults } from "@spotify/web-api-ts-sdk";
 
 export const searchTypes = ["artist", "album", "track"] as const;
 export type SearchType = (typeof searchTypes)[number];
@@ -10,16 +11,34 @@ type ValidateReturn =
       data: Data;
     }
   | { status: "invalid" };
+type Item =
+  | {
+      artist: string;
+      genres: string[];
+      image: { url: string; width: number; height: number };
+    }
+  | {
+      album: string;
+      artist: string;
+      genres: string[];
+      image: { url: string; width: number; height: number };
+    }
+  | {
+      track: string;
+      album: string;
+      artist: string;
+      genres: string[];
+      image: { url: string; width: number; height: number };
+    };
 type Results = {
   status: "ok" | "no-results";
-  items: {
-    artist: string;
-    album?: string;
-    track?: string;
-    genres: string[];
-    image: { url: string; width: number; height: number };
-  }[];
+  items: Item[];
 };
+
+const formatter = new Intl.ListFormat("en", {
+  style: "long",
+  type: "conjunction",
+});
 
 export function validate(formData: FormData): ValidateReturn {
   const type = formData.get("type") as SearchType | null;
@@ -48,24 +67,55 @@ export async function search({ type, query }: Data) {
   };
 
   const response = await fetch("https://api.spotify.com/v1/search", init);
-  const data = await response.json();
+  const data: SearchResults<["artist", "album", "track"]> =
+    await response.json();
 
   const results: Results = { status: "ok", items: [] };
 
   switch (type) {
     case "album":
-      break;
-    case "artist":
-      if (!data.artist.items.length) {
+      if (!data.albums.items.length) {
         results.status = "no-results";
       } else {
-        results.items = data.artist.items.map((item) => ({
-          artist: item.name,
-          image: item.image,
+        results.items = data.albums.items.map((item) => ({
+          album: item.name,
+          artist: formatter.format(item.artists.map((a) => a.name)),
+          image: item.images[0],
+          genres: item.genres,
         }));
       }
       break;
+
+    case "artist":
+      if (!data.artists.items.length) {
+        results.status = "no-results";
+      } else {
+        results.items = data.artists.items.map((item) => ({
+          artist: item.name,
+          image: item.images[0],
+          genres: item.genres,
+        }));
+      }
+      break;
+
     case "track":
+      if (!data.tracks.items.length) {
+        results.status = "no-results";
+      } else {
+        // results.items = data.tracks.items.map((item) => {
+        //   const genres = new Set();
+        //   item.artists.forEach((artist) =>
+        //     artist.genres.forEach((g) => genres.add(g))
+        //   );
+        //   return {
+        //     track: item.name,
+        //     artist: formatter.format(item.artists.map((a) => a.name)),
+        //     album: item.album.name,
+        //     genres: genres,
+        //     image: []
+        //   };
+        // });
+      }
       break;
   }
 
